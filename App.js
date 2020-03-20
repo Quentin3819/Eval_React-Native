192981b64612e6c2790c4a02598cc1ba96e8f59d
Quentin3819 <58207803+Quentin3819@users.noreply.github.com>
2020-03-20 11:30:25 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,73 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  Alert,
+  Button,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import AccueilContact from './src/component/AccueilContact';
+import DetailsContact from './src/component/DetailsContact';
+
+
+const Stack = createStackNavigator();
+
+function Acceuil({naviagtion}) {
+  return <AccueilContact />;
+}
+function Details({navigation, route}) {
+  navigation.setOptions({title: route.params.name});
+  return <DetailsContact student={route.params} navigation={navigation} />;
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Liste Contact"
+            component={Acceuil}
+            options={{
+              //headerMode: 'none',
+              headerShown: true,
+              headerStyle: {
+                backgroundColor: 'grey',
+              },
+              headerTintColor: 'white',
+            }}
+          />
+          <Stack.Screen
+              name="DetailsContact"
+              component={Details}
+              options={{
+                //headerMode: 'none',
+                headerShown: true,
+                headerStyle: {
+                  backgroundColor: 'grey',
+                },
+                headerTintColor: 'white',
+              }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
---
+++ src/component/AccueilContact.js
@@ -0,0 +1,96 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  FlatList,
+} from 'react-native';
+
+import {contacts} from '../../datas';
+
+export default class AccueilContact extends Component {
+  state = {
+    listcontact: [],
+  };
+  constructor(props) {
+    super(props);
+    this.state.listcontact = contacts;
+  }
+
+  render() {
+    return (
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={{flex: 1}}>
+        <SafeAreaView style={{flex: 1}}>
+          <View
+            style={{
+              flex: 1,
+            }}>
+            <View>
+              <FlatList
+                data={this.state.listcontact}
+                renderItem={({item}) => (
+                  <TouchableOpacity
+                    key={'contact_' + item.id}
+                    onPress={() => {
+                      this.props.navigation.navigate('DetailsContact', {
+                        contact: item,
+                      });
+                    }}>
+                    <View>
+                      <View
+                        style={{
+                          backgroundColor: 'black',
+                          borderBottomColor: 'white',
+                          borderBottomWidth: 1,
+                          flexDirection: 'row',
+                        }}>
+                        <View />
+                        <Image
+                          source={require('../img/unnamed.png')}
+                          style={{width: 60, height: 60, marginBottom: 20, borderRadius: 30, marginTop: 20}}
+                        />
+                        <View>
+                          <Text
+                            style={{
+                              color: 'white',
+                              marginLeft: 20,
+                              marginTop: 20,
+                              fontSize: 20,
+                            }}>
+                            {item.lastName + '-' + item.firstName}
+                          </Text>
+                          <Text
+                            style={{
+                              color: 'white',
+                              marginBottom: 20,
+                              marginLeft: 20,
+                              fontSize: 10,
+                            }}>
+                            {item.city}
+                          </Text>
+                        </View>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                )}
+              />
+            </View>
+          </View>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
