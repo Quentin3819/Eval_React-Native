e09e2c6c7d1739f462959882dce6cfebf2513015
Quentin3819 <58207803+Quentin3819@users.noreply.github.com>
2020-03-20 12:18:22 +0100

    3ieme Commit

--- src/component/DetailsContact.js
+++ src/component/DetailsContact.js
@@ -1,9 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
 import React, {Component} from 'react';
-import {Text} from 'react-native' ;
-export default class DetailsContact extends Component {
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  FlatList,
+} from 'react-native';
+
+import {contacts} from '../../datas';
+
+export default class AccueilContact extends Component {
+  state = {
+    listcontact: [],
+  };
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state.listcontact = contacts;
+    this._onPress = this._onPress.bind(this);
+  }
+  _onPress(item) {
+    this.props.navigation.navigate('DetailsContact', {
+      list: item,
+    });
+  }
   render() {
     return (
-        <Text>OK</Text>
-    )
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={{flex: 1}}>
+        <SafeAreaView style={{flex: 1}}>
+          <Text style={{fontSize: 40}}>
+            {' '}
+            Salut Batiste, désolé j'ai pas eu le Temps de faire cette page :(
+          </Text>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    );
   }
 }
